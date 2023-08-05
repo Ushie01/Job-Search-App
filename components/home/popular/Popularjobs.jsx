7d1178908aa75d656b662371687f16081e3e1057
Ushie01 <abugodwinaj@gmail.com>
2023-08-05 11:22:19 +0100

    initial commit

---
+++ components/home/popular/Popularjobs.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { useRouter } from 'expo-router';
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
+import { COLORS, SIZES } from '../../../constants';
+import styles from './popularjobs.style'
+
+
+const Popularjobs = () => {
+  return (
+    <View>
+      <Text>Popularjobs</Text>
+    </View>
+  )
+}
+
+export default Popularjobs
