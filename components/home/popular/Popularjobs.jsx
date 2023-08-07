66357d67424172ac55434ab2e4b7d118b20cbd57
Ushie01 <abugodwinaj@gmail.com>
2023-08-07 23:54:36 +0100

    create: popular and welcome section for homepage

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,16 +1,43 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useRouter } from 'expo-router';
-import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
-import { COLORS, SIZES } from '../../../constants';
-import styles from './popularjobs.style'
+import {
+	View,
+	Text,
+	TouchableOpacity,
+	FlatList,
+	ActivityIndicator,
+} from 'react-native';
+import { COLORS,  SIZES  } from '../../../constants';
+import styles from './popularjobs.style';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
 
 
 const Popularjobs = () => {
-  return (
-    <View>
-      <Text>Popularjobs</Text>
-    </View>
-  )
-}
+  const router = useRouter();
+  const isLoading = false;
+  const error = false;
+	return (
+		<View style={styles.container}>
+			<View style={styles.header}>
+        <Text>Popular jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All</Text>
+        </TouchableOpacity>
+      </View>
+      
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size='large' color={COLORS.primary} />
+        ) : error ? (<Text>Something went wrong</Text>) : <FlatList
+          data={[1, 2, 3, 4, 5, 6, 7, 8]}
+            renderItem={({ item }) => (<PopularJobCard item={item} />)}
+            keyExtractor={item => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+        />}
+      </View>
+		</View>
+	);
+};
 
-export default Popularjobs
+export default Popularjobs;
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -12,10 +12,9 @@ import { icons, SIZES } from '../../../constants';
 import { useRouter } from 'expo-router';
 
 const jobTypes = ['Full-time', 'Part-time', 'Contractor'];
-
 const Welcome = () => {
-  const router = useRouter();
-  const [activeJobType, setActiveJobType] = useState('Full-time');
+	const router = useRouter();
+	const [activeJobType, setActiveJobType] = useState('Full-time');
 
 	return (
 		<View>
@@ -47,19 +46,18 @@ const Welcome = () => {
 				<FlatList
 					data={jobTypes}
 					renderItem={({ item }) => (
-            <TouchableOpacity
-              style={styles.tab(activeJobType, item)}
-              onPress={() => {
-              setActiveJobType(item);
-              router.push(`/search/${item}`)
-              }}
-            >
+						<TouchableOpacity
+							style={styles.tab(activeJobType, item)}
+							onPress={() => {
+								setActiveJobType(item);
+								router.push(`/search/${item}`);
+							}}>
 							<Text style={styles.tabText(activeJobType, item)}>{item}</Text>
 						</TouchableOpacity>
-          )}
-          keyExtractor={item => item}
-          contentContainerStyle={{ columnGap: SIZES.small }}
-          horizontal
+					)}
+					keyExtractor={(item) => item}
+					contentContainerStyle={{ columnGap: SIZES.small }}
+					horizontal
 				/>
 			</View>
 		</View>
