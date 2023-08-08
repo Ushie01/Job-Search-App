e50273002078ac054183a128c1c2326637ae5b1a
Ushie01 <abugodwinaj@gmail.com>
2023-08-08 17:24:48 +0100

    create: Property Job Card

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { Text, View, TouchableOpacity, Image } from 'react-native';
+import styles from './popularjobcard.style';
 
-import styles from './popularjobcard.style'
 
-const PopularJobCard = () => {
-  return (
-    <View>
-      <Text>PopularJobCard</Text>
-    </View>
-  )
-}
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
+	return (
+		<TouchableOpacity
+			style={styles.container(selectedJob, item)}
+			onPress={() => handleCardPress(item)}>
+			<TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+				<Image
+					source={{ url: item.employer_logo }}
+					resizeMode='contain'
+					style={styles.logoImage}
+				/>
+      </TouchableOpacity>
+      
+			<Text
+				style={styles.companyName}
+				numberOfLines={1}>
+				{item.employer_logo}
+			</Text>
 
-export default PopularJobCard
+			<View style={styles.infoContainer}>
+				<Text style={styles.jobName(selectedJob, item)}>{item.job_title}</Text>
+			</View>
+		</TouchableOpacity>
+	);
+};
+
+export default PopularJobCard;
--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -7,35 +7,44 @@ import {
 	FlatList,
 	ActivityIndicator,
 } from 'react-native';
-import { COLORS,  SIZES  } from '../../../constants';
+import { COLORS, SIZES } from '../../../constants';
 import styles from './popularjobs.style';
 import PopularJobCard from '../../common/cards/popular/PopularJobCard';
-
+import useFetch from '../../../hooks/useFetch';
 
 const Popularjobs = () => {
-  const router = useRouter();
-  const isLoading = false;
-  const error = false;
+	const router = useRouter();
+  const { data, isLoading, error } = useFetch('search', { query: 'React develop', num_pages: 1 });
+
+	// console.log(data);
+
 	return (
 		<View style={styles.container}>
 			<View style={styles.header}>
-        <Text>Popular jobs</Text>
-        <TouchableOpacity>
-          <Text style={styles.headerBtn}>Show All</Text>
-        </TouchableOpacity>
-      </View>
-      
-      <View style={styles.cardsContainer}>
-        {isLoading ? (
-          <ActivityIndicator size='large' color={COLORS.primary} />
-        ) : error ? (<Text>Something went wrong</Text>) : <FlatList
-          data={[1, 2, 3, 4, 5, 6, 7, 8]}
-            renderItem={({ item }) => (<PopularJobCard item={item} />)}
-            keyExtractor={item => item?.job_id}
-            contentContainerStyle={{ columnGap: SIZES.medium }}
-            horizontal
-        />}
-      </View>
+				<Text>Popular jobs</Text>
+				<TouchableOpacity>
+					<Text style={styles.headerBtn}>Show All</Text>
+				</TouchableOpacity>
+			</View>
+
+			<View style={styles.cardsContainer}>
+				{isLoading ? (
+					<ActivityIndicator
+						size='large'
+						color={COLORS.primary}
+					/>
+				) : error ? (
+					<Text>Something went wrong</Text>
+				) : (
+					<FlatList
+						data={data}
+						renderItem={({ item }) => <PopularJobCard item={item} />}
+						keyExtractor={(item) => item?.job_id}
+						contentContainerStyle={{ columnGap: SIZES.medium }}
+						horizontal
+					/>
+				)}
+			</View>
 		</View>
 	);
 };
